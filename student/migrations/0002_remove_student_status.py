4c929d6c566f6a77df449aae61cc9ebf9226f1ca
mayank2478 <mayankagarwal27032002>
2023-02-17 16:21:49 +0530

    enverus assignment commit

---
+++ student/migrations/0002_remove_student_status.py
@@ -0,0 +1,16 @@
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('student', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='student',
+            name='status',
+        ),
+    ]
